6339b4d684bff2499a83e3fd7e0b2f89ba95e6d9
yume190 <yume190@gmail.com>
2017-11-20 15:12:36 +0800

    refactory json+dicttionary

--- JSONDecodeKit/JSON/JSON+Dictionary.swift
+++ JSONDecodeKit/JSON/JSON+Dictionary.swift
@@ -10,42 +10,39 @@ import Foundation
 
 extension JSON {
     public func dictionary<Key,Value:PrimitiveType>() -> [Key:Value] {
-        if let dic = self.data as? NSDictionary {
-            return dic.reduce([Key:Value](), { (dic:[Key:Value], set:(key: Any, value: Any)) -> [Key:Value] in
-                var dic = dic
-                if let k = set.key as? Key,let v = Value.decode(any: set.value) {
-                    dic[k] = v
-                }
-                return dic
-            })
+        return self.toDictionary { (any:Any) -> Value? in
+            Value.decode(any: any)
         }
-        return [Key:Value]()
     }
     
     public func dictionary<Key,Value:JSONDecodable>() -> [Key:Value] {
-        if let dic = self.data as? NSDictionary {
-            return dic.reduce([Key:Value](), { (dic:[Key:Value], set:(key: Any, value: Any)) -> [Key:Value] in
-                var dic = dic
-                if let k = set.key as? Key,let v = try? Value.decode(json: JSON(any: set.value)) {
-                    dic[k] = v
-                }
-                return dic
-            })
+        return self.toDictionary { (any:Any) -> Value? in
+            try? Value.decode(json: JSON(any: any))
         }
-        return [Key:Value]()
     }
     
     public func dictionary<Key,Value:RawRepresentable>() -> [Key:Value] where Value.RawValue:PrimitiveType {
-        if let dic = self.data as? NSDictionary {
-            return dic.reduce([Key:Value](), { (dic:[Key:Value], set:(key: Any, value: Any)) -> [Key:Value] in
-                var dic = dic
-                
-                if let k = set.key as? Key,let v = Value.decode(any: set.value) {
-                    dic[k] = v
-                }
-                return dic
-            })
+        return self.toDictionary { (any:Any) -> Value? in
+            Value.decode(any: any)
         }
-        return [Key:Value]()
+    }
+    
+    private func toDictionary<Key,Value>(valueTransform:(Any) -> Value?) -> [Key:Value] {
+        guard let dic = self.data as? NSDictionary else {
+            return [Key:Value]()
+        }
+        
+        return dic.reduce([Key:Value]()) { (result:[Key:Value], set:(key: Any, value: Any)) -> [Key:Value] in
+            guard
+            let key = set.key as? Key,
+            let value = valueTransform(set.value)
+            else {
+                return result
+            }
+            
+            var result = result
+            result[key] = value
+            return result
+        }
     }
 }
