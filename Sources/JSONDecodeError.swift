84f77726c5ae3249058bcf95b6a5e8054138ff4f
yume190 <yume190@gmail.com>
2017-11-22 16:36:01 +0800

    整理 Decode Error

--- Sources/JSONDecodeError.swift
+++ Sources/JSONDecodeError.swift
@@ -9,56 +9,70 @@
 import Foundation
 
 public enum JSONDecodeError: Error {
-    case keyNotFound(keyPath:String,curruntKey:String,json:JSON)
-    case nullValue(keyPath:String,curruntKey:String,json:JSON)
-    case typeMismatch(keyPath:String,curruntKey:String,expectType:Any,actualType:Any,value:Any,json:JSON)
+    public struct BaseInfo {
+        let keyPath:String
+        let currentKey:String
+        let json:JSON
+        var description:String {
+            return [
+                "\tKeypath : \"\(keyPath)\"",
+                "\tKey : \"\(currentKey)\""
+//                "\tjson: \(json.data ?? "nil")"
+            ].joined(separator: "\n")
+        }
+    }
+    case keyNotFound(baseInfo:BaseInfo)
+    case nullValue(baseInfo:BaseInfo)
+    case typeMismatch(baseInfo:BaseInfo,expectType:Any,actualType:Any,value:Any)
     case specialCase(reason:String)
     
     static public func produceError<T> (targetType:T.Type, json:JSON, key:String) -> JSONDecodeError {
+        let baseInfo = BaseInfo(keyPath: json.keypath, currentKey: key, json: json)
         guard let data = json[key].data else {
-            return JSONDecodeError.keyNotFound(keyPath: json.keypath, curruntKey: key, json:json)
+            return JSONDecodeError.keyNotFound(baseInfo: baseInfo)
         }
             
         if data is NSNull {
-            return JSONDecodeError.nullValue(keyPath: json.keypath, curruntKey: key, json:json)
+            return JSONDecodeError.nullValue(baseInfo: baseInfo)
         }
         
-        return JSONDecodeError.typeMismatch(keyPath: json.keypath, curruntKey: key, expectType: T.self, actualType: type(of:data),value: data, json:json)
+        return JSONDecodeError.typeMismatch(baseInfo: baseInfo, expectType: T.self, actualType: type(of:data),value: data)
     }
 }
 
 extension JSONDecodeError:CustomStringConvertible {
     public var description: String {
         switch self {
-        case .keyNotFound(let keyPath, let currentKey, let json):
-            return [
-                "",
-                "Key Not Found:",
-                "\tKeypath : \"\(keyPath)\"",
-                "\tKey : \"\(currentKey)\"",
-                "\tjson: \(json.data ?? "nil")"
-            ].joined(separator: "\n")
-        case .nullValue(let keyPath, let currentKey, let json):
-            return [
-                "",
-                "Null Value Found At:",
-                "\tKeypath : \"\(keyPath)\"",
-                "\tKey : \"\(currentKey)\"",
-                "\tjson: \(json.data ?? "nil")"
-            ].joined(separator: "\n")
-        case .typeMismatch(let keyPath, let currentKey, let expectType, let actualType,let value, let json):
-            return [
-                "",
-                "Type Mismatch:",
-                "\tKeypath : \"\(keyPath)\"",
-                "\tKey : \"\(currentKey)\"",
-                "\tExpected Type : \(expectType)",
-                "\tActual Type : \(actualType)",
-                "\tActual Value : \(value)",
-                "\tjson: \(json.data ?? "nil")"
-            ].joined(separator: "\n")
+        case .keyNotFound(let baseInfo):
+            return errorMessage(messageType: "Key Not Found::", baseInfo: baseInfo,
+                                expectType: nil, actualType: nil, value: nil)
+        case .nullValue(let baseInfo):
+            return errorMessage(messageType: "Null Value Found At:", baseInfo: baseInfo,
+                                expectType: nil, actualType: nil, value: nil)
+        case .typeMismatch(let baseInfo, let expectType, let actualType,let value):
+            return errorMessage(messageType: "Type Mismatch:", baseInfo: baseInfo,
+                                expectType: expectType, actualType: actualType, value: value)
         case .specialCase(let reason):
             return reason
         }
     }
+}
+
+fileprivate func errorMessage(messageType: String, baseInfo:JSONDecodeError.BaseInfo,
+                              expectType: Any?, actualType: Any?, value: Any?) -> String {
+    let baseResult:String = [
+        "",
+        messageType,
+        baseInfo.description
+    ].joined(separator: "\n")
+    guard let expectType = expectType, let actualType = actualType, let value = value else {
+        return baseResult
+    }
+    
+    return [
+        baseResult,
+        "\tExpected Type : \(expectType)",
+        "\tActual Type : \(actualType)",
+        "\tActual Value : \(value)",
+    ].joined(separator: "\n")
 }
