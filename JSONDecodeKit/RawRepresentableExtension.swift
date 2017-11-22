de6694a23725d8f42a53e33935528dc0b8e51d0e
yume190 <yume190@gmail.com>
2017-11-22 16:32:25 +0800

    RawRepresentable 統一 decode 格式

--- JSONDecodeKit/RawRepresentableExtension.swift
+++ JSONDecodeKit/RawRepresentableExtension.swift
@@ -13,16 +13,14 @@ import Foundation
 //}
 
 extension RawRepresentable {
-    static func decode(any: Any?) -> Self? {
-        guard let data = any as? Self.RawValue else {
-            return nil
+    public static func decode(any: Any?) throws -> Self {
+        guard let data = any as? Self.RawValue, let result = Self(rawValue: data) else {
+            throw JSONDecodeError.specialCase(reason: "...")
         }
-        return Self(rawValue: data)
+        return result
     }
     
-    // !!!!!!!
-    static func decode(json: JSON) -> Self? {
-        return decode(any: json.data)
+    public static func decode(json: JSON) throws -> Self {
+        return try decode(any: json.data)
     }
 }
-
