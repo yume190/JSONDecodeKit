f62e59df93d347fa481cef70c472201a0563ecaa
yume190 <yume190@gmail.com>
2018-03-06 11:51:14 +0800

    修正 SPM

--- Package.swift
+++ Package.swift
@@ -2,19 +2,4 @@ import PackageDescription
 
 let package = Package(
     name: "JSONDecodeKit",
-    products: [
-         .library(name: "JSONDecodeKit", type: .automatic, targets: ["JSONDecodeKit"]),
-    ],
-    targets: [
-        .target(
-            name: "JSONDecodeKit",
-            dependencies: [],
-            path: "JSONDecodeKit",
-            exclude: [
-                "Base.lproj",
-                "Info.plist",
-                "JSONDecodeKit.h"
-            ]
-        ),
-    ]
 )
